14232f40f340d2e490d978c13fe61adae5837a03
Steven Allen <steven@stebalien.com>
2013-11-23 13:39:44 -0500

    Remove '-extra-' from package name.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup, find_packages
 setup(
     name = "overkill-bar",
     version = "0.1",
-    install_requires=["overkill", "overkill-extra-writers"],
+    install_requires=["overkill", "overkill-writers"],
     packages = find_packages(),
     author = "Steven Allen",
     author_email = "steven@stebalien.com",
